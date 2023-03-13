741018a069f986585423c222c50f03520d093a5c
MaxPur <maxpur@icloud.com>
2023-03-13 14:01:47 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+axios.defaults.baseURL = "https://pixabay.com/api/";
 
 export const request = async (searchQuery, page) => {
-  const response = await axios.get("https://pixabay.com/api/?key=32897793-60dbf183924c0887bd9576dfe&q=car");
+  const response = await axios.get(`?key=32897793-60dbf183924c0887bd9576dfe&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=12&page=${page}`);
   return response.data;
 };
 
