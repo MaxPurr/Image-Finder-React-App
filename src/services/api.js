1d17305e6be9367d10b64ee9059f56a43662e65d
MaxPur <maxpur@icloud.com>
2023-03-13 13:37:00 +0300

    /

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const request = async (searchQuery, page) => {
-  const response = await axios.get('https://pixabay.com/api', {
+  const response = await axios.get('https://pixabay.com/api/', {
             params: {
                 key: '32897793-60dbf183924c0887bd9576dfe',
                 q: searchQuery,
