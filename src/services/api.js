262c7b95d8ca3b9179153ebab2eade64bcbd03b3
MaxPur <maxpur@icloud.com>
2023-03-13 13:50:04 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,8 @@
 import axios from "axios";
 
+
 export const request = async (searchQuery, page) => {
-  const response = await axios.get('https://pixabay.com/api/', {
-            params: {
-                key: '32897793-60dbf183924c0887bd9576dfe',
-                q: searchQuery,
-                image_type: 'photo',
-                orientation: 'horizontal',
-                safesearch: true,
-                per_page: 12,
-                page: page,
-            }
-  });
+  const response = await axios.get("https://pixabay.com/api/?key=32897793-60dbf183924c0887bd9576dfe&q=car");
   return response.data;
 };
 
